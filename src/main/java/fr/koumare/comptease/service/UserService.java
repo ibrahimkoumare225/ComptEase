5b5ef2246d94195a14d80e072da16503be19678b
Koumare <ibrahimkoumare5@gmail.com>
2025-02-27 21:34:22 +0100

    Finalisation de la page d'authentification

--- src/main/java/fr/koumare/comptease/service/UserService.java
+++ src/main/java/fr/koumare/comptease/service/UserService.java
@@ -2,15 +2,9 @@ package fr.koumare.comptease.service;
 
 import fr.koumare.comptease.model.User;
 
-import java.util.List;
-
 public interface UserService {
 
-    public void addUser(User user) ;
+    public boolean registerUser(String pseudo, String email, String password) ;
+    public boolean authenticateUser(String pseudo, String password);
 
-    public List<User> getAllUser() ;
-
-    public void updateUser(User user);
-
-    public void deleteUser(Long id) ;
 }
